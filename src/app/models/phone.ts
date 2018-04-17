a8ce442a028033a0d030a265377291accda28032
João Sizílio <jvcsizilio@gmail.com>
2018-04-17 08:59:34 -0300

    Fix phone

--- src/app/models/phone.ts
+++ src/app/models/phone.ts
@@ -2,5 +2,5 @@ export class Phone{
     // 1 vivo, 2 tim, 3 claro, 4 oi, 5 nextel, 6 outros
     public phone: string;
     public mno: string;
-    public has_whatsapp = 0;
+    public whatsapp = 0;
 }
